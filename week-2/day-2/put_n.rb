8576a73802a15f965d87067f8efc8b47a7cba817
Rai <raikcuerda19@gmail.com>
2020-04-20 18:28:38 -0500

    put_2

--- week-2/day-2/put_n.rb
+++ week-2/day-2/put_n.rb
@@ -1,8 +1,22 @@
-def put_n(message:"",time:0)
-    if time.integer? && time > 0
-        time.times{puts "#{message} #{time}"}
-    else
-        raise ArgumentError.new("Value invalid")
+def put_n()
+    loop do
+        puts "Put the string that you want repeat"
+        message = gets.chomp
+        puts "Put the times that you want repeat"
+        time = gets.to_i
+
+        if time.integer? && time > 0
+            time.times{puts "#{message}"}
+            break
+        else
+            begin
+                raise ("value invalie times")
+            rescue 
+                puts "I don't understand, how many times do you want to echo the string"
+                put_n()
+            end
+        end
     end
 end
-put_n(time:5,message:"This will be printed 5 times")
+
+put_n()
