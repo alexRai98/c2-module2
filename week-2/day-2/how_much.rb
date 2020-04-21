e29c0e40da81d1f07e6b362fd913414faf6d3449
Rai <raikcuerda19@gmail.com>
2020-04-21 08:58:34 -0500

    fist

--- week-2/day-2/how_much.rb
+++ week-2/day-2/how_much.rb
@@ -1,21 +1,54 @@
 def howMuch ()
+    cadena = []
     elements =[]
-    cadena =[]
     for i in 0..1
         puts "string"
         string=gets.chomp
-        elements<< string.split(",")
+        elements << string.split(",")
     end
-    elements.inject({}){ |resul,element|
        
-        resul[:category] = element[0].strip
-        resul[:price] = element[1].strip
-        resul[:stock] = element[2].strip
-        resul[:amount]=element[3].strip
-        resul[:name] = element[4].strip
-       puts resul
-       # cadena << resul
-    }    
-    #puts cadena
+        resul = {}
+        elements.each {|element| 
+            resul[:category] = element[0].strip
+            resul[:price] = change(element[1].strip)
+            resul[:stock] = element[2].strip
+            resul[:amount]= element[3].strip
+            resul[:name] = element[4].strip
+            cadena << resul
+        }
+
+    total_USD(cadena)
+
+end
+def total_USD(products)
+    stock_products ={}
+    total = products.inject(0){|total,item|
+        
+        if item[:stock] == "true"
+            price = (item[:price].split(" "))[1].to_f
+            total += (price * item[:amount].to_i)
+            p item[:name]
+            stock_products[item[:name]] = price
+        end
+    }
+    p stock_products
+    max=stock_products.values.max
+    p max
+    product = stock_products.has_value? (max)
+
+    puts "Total almacén: #{total.round(2)}"
+    puts "El producto con mayor stock : #{product}"
+end
+def change(money)
+    string_money = money.split(" ")
+    type_money = string_money[0]
+    many_money = string_money[1].to_f
+    case type_money
+    when "PEN"
+        many_money = many_money/3
+    when "ARS"
+        many_money=many_money/40
+    end
+    return "#{type_money} #{many_money.round(2)}"
 end
 howMuch()
